809888b217f06614b28579694c61cd8a9f7c75c1
ReMatrixed <mat.vasenin@yandex.ru>
2024-10-26 17:33:31 +0300

    Remove logger.py module

--- core/logger.py
+++
@@ -1,6 +0,0 @@
-# Подключение встроенной библиотеки логгирования
-import logging
-
-# Настройка логгера
-internal_log = logging.getLogger(__name__)
-logging.basicConfig(filename = "starstorm.log", level = logging.DEBUG)

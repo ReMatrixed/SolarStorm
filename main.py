27f488cae6f964425938604553e505b441f27c3f
ReMatrixed <mat.vasenin@yandex.ru>
2024-10-26 17:33:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-# === Встроенные библиотеки
-# Библиотека для парсинга аргументов (путь до файла настроек, например)
+# Подключение встроенных библиотек
 import argparse
-
-# Прочие библиотеки
+import logging
 import json
 
 # Подключение внутренних модулей
-from core.db import prepare_database
+from core.db import DatabaseDispatcher
 
-# === Внешние библиотеки
-# Библиотека для взаимодействия с Telegram API
-from aiogram import Dispatcher
+# Инициализация парсера аргументов командной строки
+arg_parser = argparse.ArgumentParser(
+    prog = "StarStorm Telegram-Bot",
+    description = "Телеграм-бот для чат-центра Светогоской Школы"
+)
+arg_parser.add_argument("-c", "--config", required = True)
+args = arg_parser.parse_args()
 
-# Инициализация т.н. Диспетчера (корневого Роутера) для работы с ботом
-dp = Dispatcher()
+with open(args.config, "r") as config_file:
+    cfg = json.load(config_file)
 
-if __name__ == "main":
-    prepare_database()
+logging.basicConfig(filename = "starstorm.log", encoding = "utf-8")
+logger = logging.getLogger(__name__)
+
+db = DatabaseDispatcher(
+    cfg.get("db.host"),
+    cfg.get("db.port"),
+    cfg.get("db.username"),
+    cfg.get("db.password"),
+    cfg.get("db.name"),
+    logger
+)
+
+if __name__ == "__main__":
+    logger.info("Запуск бота, подождите...")
+    db.prepare_database()
+    entries = db.get_entries(-1)
+    print(entries)
+    db.close_database()

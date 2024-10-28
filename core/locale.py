92ce7bc654aa7f8457626226cfaaec4a90364bb4
ReMatrixed <mat.vasenin@yandex.ru>
2024-10-28 21:06:55 +0300

    Improve code quality of locale.py module

--- core/locale.py
+++ core/locale.py
@@ -1,16 +1,18 @@
 # Подключение библиотеки для работы с файлами локализации (.json)
 import json
 
+# Подключение библиотеки логгирования
 import logging
 
 # Класс для обработчика локализации
 class LocalizationDispatcher:
     # Словарь для хранения строк сообщений
     messages: dict
+    # Объект логгера
     logger: logging.Logger
 
     # Инициализация модуля состоит в чтении файла локализации 
-    def __init__(self, filename: str, logger: logging.Logger):
+    def __init__(self, filename: str, logger: logging.Logger) -> None:
         self.logger = logger
         self.logger.info("Подготовка диспетчера локалиизаций...")
         self.logger.info(f"Чтение файла: {filename}...")
@@ -19,9 +21,11 @@ class LocalizationDispatcher:
         self.logger.info("Диспетчер локализаций инициализирован.")
 
     # Получение значения по ключу
-    def get_str(self, key: str):
+    def get_str(self, key: str) -> str:
         source_str = self.messages.get(key)
         if(source_str != None):
+            # Если значение существует, функция возвращает его
             return source_str
         else:
+            # Если значение не найдено в файле локализации, возвращается "заглушка"
             return "UNKNOWN"
